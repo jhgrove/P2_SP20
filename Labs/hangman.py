2e6043ce0362c49a9f77d4cd0590f76f1c7f9039
jamesgrove <jgrove@fwparker.org>
2020-02-08 02:17:38 -0600

    2/7/20

--- Labs/hangman.py
+++ Labs/hangman.py
@@ -5,23 +5,6 @@ Hangman Lab
 
 import random
 
-# PSEUDOCODE
-# setup your game by doing the following
-# make a word list for your game
-# grab a random word from your list and store it as a variable
-
-# in a loop, do the following
-# display the hangman using the gallows
-# display the used letters so the user knows what has been selected
-# display the length of the word to the user using blank spaces and used letters
-# prompt the user to guess a letter
-# don't allow the user to select the same letter twice
-# if the guess is incorrect increment incorrect_guesses by 1
-# if the incorrect_guesses is greater than 8, tell the user they lost and exit the program
-# if the user gets all the correct letters, tell the user they won
-
-# ask if they want to play again
-
 gallows = [
     '''
       +---+
@@ -88,45 +71,63 @@ gallows = [
     '''
 ]
 
-# don't allow the user to select the same letter twice
-# if the guess is incorrect increment incorrect_guesses by 1
-# if the incorrect_guesses is greater than 8, tell the user they lost and exit the program
-# if the user gets all the correct letters, tell the user they won
-
-# ask if they want to play again
+word_list = [
+    "ABOOD", "AUSTIN", "AYMAR", "BANKS", "BARR", "BIGELOW", "BRANDON", "BRUNO", "CASTALDI", "CHILDREY",
+    "COLLINS", "CONLON", "DAVIS", "DENIEN", "DONOHUE", "DRUGER", "ELLIOTT", "GIBSON", "GREENSTONE", "IACONIANNI",
+    "LAUFER", "LEE", "LESAK", "LESINSKI", "LEVINE", "MAHANY", "MARKER", "OLT", "POPE", "PRITIKIN",
+    "RIFF", "SABIR", "SCHUPP", "SHEPARD", "STARKS", "TYLER", "VILLAGOMEZ", "WEBSTER", "WILSON", "ZAREMBA",
+    "ZELLER", "ZHANG"
+]
 
 done = False
-
-word_list = ["JAMES", "HENRY", "GROVE"]
-# my_word = word_list[random.randrange(3)]
-my_word = "JAMES"
+my_word = word_list.pop(random.randrange(len(word_list)))
+number_of_letters = len(my_word)
 wrong_guesses = 0
-
-letters = [chr(x) for x in range(65, 65 + 26)]
 used_letters = []
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-'''
-num_of_spaces = len(my_word)
-for i in range(num_of_spaces):
-    print("__ ", end="")
-'''
+print("\n" + my_word)
 
+print("\nWelcome to Hangman - Teachers Edition!")
+print("This teacher's name has", number_of_letters, "letters.")
 
-def hangman():
-    blank_1 = "__"
-    blank_2 = "__"
-    blank_3 = "__"
-    blank_4 = "__"
-    blank_5 = "__"
-    print(
-        blank_1, blank_2, blank_3, blank_4, blank_5
-    )
+while not done:
+    print(gallows[wrong_guesses])
+    for letter in my_word:
+        if letter in used_letters:
+            print(letter, end=" ")
+        else:
+            print("_", end=" ")
+    print()
 
-    while not done:
-        print(gallows[wrong_guesses])
-        guess = input("\nGuess a Letter: ")
-        for letter in my_word:
-            if guess.upper() == letter:
-                if guess.upper() == "A":
-                    blank_2 = "A"
-                used_letters.append(letter)
+    if number_of_letters > 0:
+        print("\nUsed Letters:", used_letters)
+        chosen_letter = input("Pick a letter: ").upper()
+        number_of_letters = number_of_letters - my_word.count(chosen_letter)
+
+        if chosen_letter in used_letters:
+            number_of_letters = number_of_letters + my_word.count(chosen_letter)
+            print("\nYou already guessed that letter! Choose a different letter.")
+
+        elif chosen_letter in my_word:
+            used_letters.append(chosen_letter)
+            print("\nNice job! The letter", chosen_letter, "is in your word!")
+
+        elif len(chosen_letter) != 1:
+            print("\nYou must guess one letter at a time!")
+
+        elif chosen_letter not in my_word:
+            used_letters.append(chosen_letter)
+            if wrong_guesses < 6:
+                wrong_guesses += 1
+                print("\nSorry,", chosen_letter, "is not in your word.")
+            if wrong_guesses >= 6:
+                print("YOU LOSE. The word was " + my_word)
+                done = True
+
+        elif chosen_letter not in letters:
+            print("\nThat's not a letter. Please choose a letter.")
+
+    elif number_of_letters == 0:
+        print("\nYOU WIN! The word was", my_word + ". Thanks for Playing!")
+        done = True
