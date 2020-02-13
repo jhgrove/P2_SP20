4f31499cb89bee519b5227ec3d1642e36c1bcf03
jamesgrove <jgrove@fwparker.org>
2020-02-13 16:21:02 -0600

    2/13/20

--- Problems/Searching/search_problems.py
+++ Problems/Searching/search_problems.py
@@ -31,6 +31,7 @@ for word in my_list:
     x += 1
 print(longest_word)
 
+
 # 2.  (8pts) Write code which finds the total word count AND average word length in "AliceInWonderLand.txt"
 file2 = open("../Searching/AliceInWonderLand.txt")
 my_list2 = []
@@ -40,27 +41,40 @@ for line2 in file2:
     words2 = split_line(line2)
     for word2 in words2:
         my_list2.append(word2)
-print("Total Word Count:", len(my_list2), "words.")
+print("Total Word Count:", len(my_list2), "words")
 
 total_length = 0
 for i in my_list2:
     word_len = len(i)
     total_length += word_len
 
-print("Average Word Length:", total_length // len(my_list2), "letters.")
+print("Average Word Length:", total_length // len(my_list2), "letters")
+
 
 # 3.  (3pts) How many times does the name Alice appear in Alice in Wonderland?
 num_of_alice = 0
 for i in my_list2:
     if i == "ALICE":
         num_of_alice += 1
-print("Alice was found", num_of_alice, "times.")
+print("Alice Found:", num_of_alice, "times")
+
 
 # 4.  (6pts) Find the most frequently occurring seven letter word in "AliceInWonderLand.txt"
+seven_list = []
+
 for i in my_list2:
-    if len(i) != 7:
-        del my_list2[my_list2.index(i)]
-print(my_list2)
+    if len(i) == 7:
+        seven_list.append(i)
+
+appearances = 0
+seven_word = 0
+
+for n in seven_list:
+    if seven_list.count(n) > appearances:
+        appearances = seven_list.count(n)
+        seven_word = n.lower()
+
+print("Most Common Seven Letter Word:", seven_word.capitalize())
 
 
 # 5.  (2pts, small points challenge problem)
