25c2ac8a67c506318c072e173f95dc90e1038d86
jamesgrove <jgrove@fwparker.org>
2020-02-13 13:46:10 -0600

    2/13/20

--- Problems/Searching/search_problems.py
+++ Problems/Searching/search_problems.py
@@ -1,26 +1,24 @@
-'''
+"""
 Searching problems (25pts)
 Complete the following 3 searching problems using techniques
 from class and from the notes and the textbook website.
 Solutions should use code to find and print the answer.
-'''
+"""
 import re
+
 
 def split_line(line):
     # uses regular expressions to split line of text into word list
     return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line)
 
-
-# 1.  (6pts) Write code which finds and prints the longest
-# word in the provided dictionary.  If there are more
-# than one longest word, print them all.
+# 1.  (6pts) Write code which finds and prints the longest word in the provided dictionary. If there are more than
+# one longest word, print them all.
 
 
-# 2.  (8pts)  Write code which finds the total word count AND average word length
-# in "AliceInWonderLand.txt"
+# 2.  (8pts) Write code which finds the total word count AND average word length in "AliceInWonderLand.txt"
 
 
-# 3.  (3pts)  How many times does the name Alice appear in Alice in Wonderland?
+# 3.  (3pts) How many times does the name Alice appear in Alice in Wonderland?
 
 
 # 4.  (6pts) Find the most frequently occurring seven letter word in "AliceInWonderLand.txt"
@@ -30,6 +28,3 @@ def split_line(line):
 # How many times does "Cheshire" occur in"AliceInWonderLand.txt"?
 # How many times does "Cat" occur?
 # How many times does "Cheshire" immediately followed by "Cat" occur?
-
-
-
