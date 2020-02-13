9a13ef8bb7773538b360d1fe7e7b6e40dc583311
jamesgrove <jgrove@fwparker.org>
2020-02-13 15:46:32 -0600

    2/13/20

--- Problems/Searching/search_problems.py
+++ Problems/Searching/search_problems.py
@@ -26,17 +26,32 @@ for line in file:
 print(my_list)
 
 for word in my_list:
-    if len(word[x]) >= longest_word:
-        longest_word = len(word[x])
-        x += 1
-    else:
-        x += 1
+    if len(word) >= longest_word:
+        longest_word = len(word)
+    x += 1
 print(longest_word)
 
 # 2.  (8pts) Write code which finds the total word count AND average word length in "AliceInWonderLand.txt"
+file2 = open("../Searching/AliceInWonderLand.txt")
+my_list2 = []
 
+for line2 in file2:
+    line2 = line2.strip().upper()
+    words2 = split_line(line2)
+    for word2 in words2:
+        my_list2.append(word2)
+print(my_list2)
+print("Total Word Count:", len(my_list2), "words.")
+
+total_length = 0
+for i in my_list2:
+    word_len = len(i)
+    total_length += word_len
+
+print("Average Word Length:", total_length // len(my_list2), "letters.")
 
 # 3.  (3pts) How many times does the name Alice appear in Alice in Wonderland?
+
 
 
 # 4.  (6pts) Find the most frequently occurring seven letter word in "AliceInWonderLand.txt"
