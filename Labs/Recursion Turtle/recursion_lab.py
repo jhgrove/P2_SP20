a4e1bb1a5bb177cd4260105cb6cd089931438e09
jamesgrove <jgrove@fwparker.org>
2020-03-09 10:26:48 -0500

    3/9/20

--- Labs/Recursion Turtle/recursion_lab.py
+++ Labs/Recursion Turtle/recursion_lab.py
@@ -18,4 +18,33 @@ Snowflakes, trees, and spirals are a common choice.  Or you can create a third o
 
 """
 
-# PART 1 - H Fractal
+import turtle
+
+my_turtle = turtle.Turtle()
+my_turtle.showturtle()
+my_turtle.shape('turtle')
+my_turtle.speed(0)
+my_screen = turtle.Screen()
+my_screen.bgcolor('white')
+
+
+# PART 1 - H Tree
+
+
+def h_tree(x, y, height, width, depth):
+    if depth > 0:
+        for i in range(2):
+            my_turtle.up()
+            my_turtle.goto(x, y)
+            my_turtle.down()
+            my_turtle.goto(x + width / 4, y)
+            my_turtle.goto(x + width / 4, y + height / 4)
+            my_turtle.goto(x + width / 4, y - height / 4)
+            h_tree(x + width / 4, y - height / 4, height / 2, width / 2, depth - 1)
+            h_tree(x + width / 4, y + height / 4, height / 2, width / 2, depth - 1)
+            height, width = - height, - width
+
+
+h_tree(0, 0, my_screen.window_height(), my_screen.window_width(), 5)
+
+my_screen.exitonclick()
