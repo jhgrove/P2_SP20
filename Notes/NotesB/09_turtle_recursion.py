ea8a64d7dacf415f1830285c2735e378a69dcaab
jamesgrove <jgrove@fwparker.org>
2020-03-09 09:52:24 -0500

    3/9/20

--- Notes/NotesB/09_turtle_recursion.py
+++ Notes/NotesB/09_turtle_recursion.py
@@ -1,6 +1,6 @@
-'''
+"""
 Turtle is an introductory graphics program for learning.
-'''
+"""
 
 import turtle
 
@@ -17,9 +17,9 @@ my_screen.bgcolor('white')
 # draw a shape using goto method (red square)
 my_turtle.fillcolor('red')
 my_turtle.begin_fill()  # starts a shape which will be filled
-my_turtle.goto(200, 0) # bottom right
-my_turtle.goto(200, 200) # top right
-my_turtle.goto(0, 200) # top left
+my_turtle.goto(200, 0)  # bottom right
+my_turtle.goto(200, 200)  # top right
+my_turtle.goto(0, 200)  # top left
 my_turtle.goto(0, 0)  # back to bottom left
 my_turtle.end_fill()
 
@@ -31,7 +31,7 @@ my_turtle.setheading(270)  # face south
 my_turtle.fillcolor('blue')
 my_turtle.begin_fill()
 
-# draw octogon
+# draw octagon
 for i in range(8):
     my_turtle.forward(50)
     my_turtle.left(45)
@@ -40,6 +40,7 @@ my_turtle.end_fill()
 
 # Recursive rectangle pattern
 my_screen.clear()
+
 
 def recursive_rect(width, height, depth):
     if depth > 0:
@@ -52,6 +53,7 @@ def recursive_rect(width, height, depth):
         my_turtle.goto(width / 2, -height / 2)  # bottom right
         my_turtle.goto(width / 2, height / 2)  # back to beginning
         recursive_rect(width / 1.5, height / 1.5, depth - 1)
+
 
 recursive_rect(800, 500, 10)
 my_screen.clear()
@@ -71,13 +73,12 @@ def recursive_ncaa(x, y, height, width, depth):
         my_turtle.forward(width)
         my_turtle.left(90)
         my_turtle.forward(height)
-        my_turtle.left(90) # start bottom bracket
+        my_turtle.left(90)  # start bottom bracket
         my_turtle.forward(width)
         recursive_ncaa(x + width, y + height / 2, height / 2, width, depth - 1)  # top bracket
         recursive_ncaa(x + width, y - height / 2, height / 2, width, depth - 1)
 
+
 recursive_ncaa(-300, 0, 300, 100, 5)
-
-
 
 my_screen.exitonclick()
