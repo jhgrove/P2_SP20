5c20fecf15e9aa726760c6878caee7f3693534af
jamesgrove <jgrove@fwparker.org>
2020-03-25 13:08:38 -0500

    3/25/20

--- Labs/CTA Ridership/cta_rider_lab.py
+++ Labs/CTA Ridership/cta_rider_lab.py
@@ -1,4 +1,4 @@
-'''
+"""
 CTA Ridership (25pts)
 
 Get the csv from the following data set.
@@ -12,8 +12,9 @@ It has been updated with 2018 data, but not yet with 2019 unfortunately
 3  Plot total usage on a third line on your graph. (5pts)
 4  Add a title and label your axes. (4pts)
 5  Add a legend to show data represented by each of the three lines. (4pts)
-6  What trend or trends do you see in the data?  Offer a hypotheses which might explain the trend(s). Just add a comment here to explain. (2pts)
-'''
+6  What trend or trends do you see in the data?  Offer a hypotheses which might explain the trend(s).
+   Just add a comment here to explain. (2pts)
+"""
 
 import csv
 
@@ -22,3 +23,21 @@ with open("CTA_-_Ridership_-_Annual_Boarding_Totals (1).csv") as f:
     data = list(cr)
 
 print(data)
+
+headers = data.pop(0)
+print(headers)
+
+years = [x[0] for x in data]
+bus = [x[1] for x in data]
+rail = [x[3] for x in data]
+total = [x[4] for x in data]
+
+print(years[-10:])
+print(bus[-10:])
+print(rail[-10:])
+print(total[-10:])
+
+# axis
+# labels
+# title
+# legend (label plots)
