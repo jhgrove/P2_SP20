fa7e246f76dc155c8dbdeed211d8d14cf256c679
jamesgrove <jgrove@fwparker.org>
2020-04-20 11:05:55 -0500

    4/20/20

--- Notes/NotesB/import_me/11_scraping.py
+++ Notes/NotesB/import_me/11_scraping.py
@@ -18,3 +18,40 @@ print(soup.prettify())  # nicer way to look at it
 title = soup.find("title")  # title is the tag name
 print(title)
 print(title.text)
+
+# by attribute
+print("\n" * 10)
+authors = soup.find_all(itemprop="author")
+print(authors)
+print(type(authors))
+
+for author in authors:
+    print(author.text)
+
+# by css class
+print("\n" * 10)
+quotes = soup.find_all(class_="quote")
+print(quotes[0].prettify())
+
+# drill down using dot notation
+print(quotes[0].span)
+print(quotes[0].span.text)
+
+for quote in quotes:
+    print(quote.span.text)
+
+
+# use author and quotes together
+for i in range(len(quotes)):
+    print(quotes[i])
+    print("\t\t---{}".format(authors[i].text))
+    print()
+
+
+# You can also combine search terms
+
+quotes = soup.find_all("span", class_="text", itemprop="text")
+
+for quote in quotes:
+    print(quote.text)
+
